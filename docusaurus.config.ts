93068054de336d13ed7eea1f5316aecd3245692f
Alex Wright Laptop WSL <alex@wrightaero.space>
2025-07-09 18:56:04 +0930

    Refactor site branding and layout: Update site title to 'Home', change logo alt text to 'AFT Dynamics Logo', adjust feature layout in homepage, and modify CSS color variables for a new theme.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -85,9 +85,9 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'My Site',
+      title: 'Home',
       logo: {
-        alt: 'My Site Logo',
+        alt: 'AFT Dynamics Logo',
         src: 'img/logo.svg',
       },
       items: [
--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -20,31 +20,11 @@ const FeatureList: FeatureItem[] = [
       </>
     ),
   },
-  {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/logo.svg').default,
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/logo.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
-  },
 ];
 
 function Feature({title, Svg, description}: FeatureItem) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx('col col--6 col--offset-3')}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
